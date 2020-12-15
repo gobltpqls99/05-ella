a01a21b5ef8a218fa6f8e06341ba0d51f382bc9e
gobltpqls99 <gobltpqls99@naver.com>
2020-12-15 11:00:26 +0900

    lottoA완성

--- js/lotto.js
+++ js/lotto.js
@@ -1,37 +1,76 @@
 /**
- ** A 알고리즘 
- ** 1. 1~45까지의 숫자 중 원하는 번호를 생성한다. - Math.random() 
- ** 2. 추출된 번호를 lotto 배열에 넣는다. 단 중독X
- ** 3. 6개의 번호가 추출되면 .resuly-wrap 에 공을 만들어 넣는다.
-
- ** B 알고리즘
- ** 1. 1~45까지의 번호가 존재하는 배열이 있다.
- ** 2. numbers에서 6개의 번호를 ㅊ출하여 lotto 배열에 넣는다.
- ** 3. 6개의 번호가 추출되면 .resuly-wrap 에 공을 만들어 넣는다.
-
- var numbers = [];
- for (var i=0; i<=45; i++) numbers.push(i)
- $(".result-wrap").empty();
- var lotto = [];
+ *! A 알고리즘
+ * 1. 1 ~ 45까지의 숫자 중 원하는 번호를 생성한다 - Math.random()
+ * 2. 추출된 번호를 lotto배열에 넣는다. 단 중복되면 안된다.
+ * 3. 6개의 번호가 추출되면 .result-wrap에 공을 만들어 넣는다.
+ * 
+ *! B 알고리즘
+ * 1. 1~45까지의 번호가 존재하는 numbers 배열이 있다.
+ * 2. numbers에서 6개의 번호를 추출하여 lotto배열에 넣는다.
+ * 3. 6개의 번호가 추출되면 .result-wrap에 공을 만들어 넣는다.
+ * 
+ *? 다 만들면 booldook@gmail.com으로 폴더 전체를 압축하여 보낸다.
+ * 보내는 제목 : [홍길동] 로또 숙제
  */
- 
+/***************************** *****************************/
+var lotto = []; // 로또 번호를 담을 배열
+var legacyLotto = []; // 기존 로또 번호를 담을 배열
+
+function colorSel(n) {
+	if (n <= 10) return 'yellow';
+	else if (n <= 20) return 'blue';
+	else if (n <= 30) return 'red';
+	else if (n <= 40) return 'gray';
+	else return 'green';
+}
+/***************************** *****************************/
+function genLottoHtml(arr) {
+	// for(var i=0; i<lotto.length; i++) {
+	var html = '';
+	for (var i in arr) { // 공을 생성해서 화면에 그린다.
+		html += ('<div class="number ' + colorSel(arr[i]) + '">' + arr[i] + '</div>');
+	}
+	return html;
+}
+
+
+/***************************** *****************************/
 function onLucky() {
-	var lotto = [0,0,0,0,0,0];
-	var num;
-	var i=0
-	$(".result-wrap").empty();
-	// for (var i=0; i<=45; i++) ;
-	do {
-		num =Math.floor(Math.random()*45)+1;
-		for (var j=0; j<=i; j++){
-			if (num===lotto[j]) break;
-		}
-		if (j<i) continue;
-		lotto[i]=num;
-		i++;
-	} while (i<6);
+	legacyLotto = lotto;
+	lotto = [];
+	var number; // 추출한 번호를 담을 변수
+	var color; // 생성될 공의 class를 담을 변수
 
-	for (var i=0; i<6; i++)document.write(lotto[i] +",");
 
-};
+	/***************************** *****************************/
+	while (lotto.length < 6) { // 6개의 랜덤한 번호를 lotto에 넣는다.
+		number = Math.floor(Math.random() * 45) + 1;
+		if (lotto.indexOf(number) == -1) lotto.push(number);
+	}
+
+	/***************************** *****************************/
+	lotto.sort(function (a, b) { // lotto를 오름차순으로 정렬한다.
+		return a - b; // 오름차순
+		// return b - a; // 내림차순
+	});
+	/***************************** *****************************/
+	$(".result-wrap").empty().append(genLottoHtml(lotto)); // 기존의 번호를 지우고 공을 생성 후 화면에 표시
+
+	/***************************** *****************************/
+	if (legacyLotto.length > 0) {
+		html = '<div class="history">';
+	}
+
+	/***************************** *****************************/
+	for (var i in legacyLotto) { // 공을 생성해서 화면에 그린다.
+		html += '<div class="number ' + colorSel(legacyLotto[i]) + '">' + legacyLotto[i] + '</div>';
+	}
+	/***************************** *****************************/
+	$(".history-wrapper").prepend(html);
+	html += genLottoHtml(legacyLotto);
+	html += '</div>';
+
+}
+
+
 $("#btLucky").click(onLucky);
