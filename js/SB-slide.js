ae491635ed4c8cd9b6337a9adda2c672a99e20ec
gobltpqls99 <gobltpqls99@naver.com>
2020-12-16 18:22:49 +0900

    pager

--- js/SB-slide.js
+++ js/SB-slide.js
@@ -3,7 +3,7 @@
 var container = '.SB-slide';
 var stageDefault = 4; //스테이지에 보여질 개수 기준값(pc)
 var moveCnt = 1;	// 한번에 움직여지는 슬라이드 개수
-var speed = 500;
+var speed = 300;
 /**
  * 전역변수 
  * */
@@ -13,19 +13,21 @@ var $wrapper = $container.find('.slide-wrapper');
 var $slide = $container.find('.slide');
 var $BtPrev = $container.find('.bt-prev');
 var $BtNext = $container.find('.bt-next');
+var $pagerWrapper = $container.find('.pager-wrapper');
 var slideCnt = $slide.length;	// 슬라이드의 총 개수 length
 var slideLast = slideCnt - 1; // 슬라이드의 마지막 index 
 var slideWid;	// 슬라이드의 width
 var stageView;	// 스테이지에 보여질 개수의 반응형에 따른 변하는 값
 var now = 0;
 var direction = 1; // 1: 오른쪽으로 이동 Prev //-1: 왼쪽으로 이동 Next
-
+var target; // Animation($wrapper) 될 left 값
 
 /**
  * 사용자 함수
  * */
 function init() {
 	$wrapper.empty().css("left",0);
+	$pagerWrapper.find('.pager').removeClass('active').eq(now).addClass('active');
 	$slide.eq(now).clone().appendTo($wrapper).css("width", slideWid+"%");
 	for(var i=0, my, prev=now; i<stageView; i++) {
 		prev = my = (prev == 0) ? slideLast : prev - 1;
@@ -38,7 +40,7 @@ function init() {
 }
 
 	function slideAni() {
-		$wrapper.stop().animate({"left": direction * slideWid * moveCnt + "%"}, speed, init);
+		$wrapper.stop().animate({"left": target}, speed, init);
 	}
 
 /**
@@ -57,25 +59,47 @@ function onResize() {
 }
 
 function onPrev() {
-	direction = 1;
+	// direction = 1;
+	target = slideWid * moveCnt + "%";
 	now = now == slideLast ? 0 : now - 1
 	slideAni();
 }
 
 function onNext() {
-	direction =-1;
+	// direction =-1;
+	target = -slideWid * moveCnt + "%";
 	now = now == slideLast ? 0 : now + 1
 	slideAni();
+}
+
+function onPager() {
+	var old = now;
+	now = $(this).index();
+	if(old > now) {
+		target = '100%'
+	//그림교체
+	slideAni();
+}
+else if(old < now) {
+	target='-100%'
+	//그림교체
+	slideAni();
+	}
 }
 
  /**
   * 이벤트 등록 
   * */
- $(window).resize(onResize).trigger("resize");
  $BtPrev.click(onPrev);
  $BtNext.click(onNext);
-
-
+ /*
+  var i =0; i<slideCnt; i++ 
+ var i in $slide
+ */
+ for(var i =0; i<slideCnt; i++ ) 
+	$('<i class="pager"></i>').appendTo($pagerWrapper).click(onPager);
+ 
+ $(window).resize(onResize).trigger("resize");
 
 
 
